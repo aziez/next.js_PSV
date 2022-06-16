d7e4e056195150ede9bca9b67fd235ccecaf0723
Aziez <Aziz13.id@gmail.com>
2022-06-16 20:58:03 +0700

    Tambaah Api Data

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Nodes } from "../data/Node";
+import { marker } from "../data/Marker";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+export default function handler(req, res) {;
+    
+  res.status(200).json({Nodes});
 }
